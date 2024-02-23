242d2bd8ce60023e3191b9276f60f993d6248294
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-02-23 09:42:05 -0800

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # install flask from pip3.
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
