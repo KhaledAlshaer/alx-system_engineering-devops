3f1bd8ce6d56fcde7a024c0448bdfd0f8a008343
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-05-18 02:05:17 -0700

    0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == '__main__':
+
+    link = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    params = {'userId': employee_id}
+
+    request = requests.get(f"{link}/users/{employee_id}").json()
+    todo = requests.get(f"{link}/todos", params=params).json()
+    completed = [td['title'] for td in todo if td['completed']]
+
+    name = request.get("name")
+    all_todo = len(todo)
+    complete = len(completed)
+
+    print(f"Employee {name} is done with tasks ({complete}/{all_todo})")
+
+    #Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
