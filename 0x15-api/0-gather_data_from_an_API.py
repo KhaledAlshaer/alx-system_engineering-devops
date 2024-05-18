06fd8c3cba8c85c6b6b38030cdd2a9c4c55da50e
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-05-18 02:17:14 -0700

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,12 +11,17 @@ if __name__ == '__main__':
     employee_id = sys.argv[1]
     params = {'userId': employee_id}
 
-    request = requests.get(f"{link}/users/{employee_id}").json()
-    todo = requests.get(f"{link}/todos", params=params).json()
+    request = requests.get(f"{link}/users/{employee_id}")
+    user_data = request.json()
+    name = user_data.get("name")
+
+    todo = requests.get(f"{link}/todos", params=params)
+    todo_data = todo.json()
+
     completed = [td['title'] for td in todo if td['completed']]
 
-    name = request.get("name")
-    all_todo = len(todo)
+    #name = request.get("name")
+    all_todo = len(todo_data)
     complete = len(completed)
 
     print(f"Employee {name} is done with tasks ({complete}/{all_todo})")
