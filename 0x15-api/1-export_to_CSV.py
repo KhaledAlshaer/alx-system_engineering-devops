eb847f0a854e45ebeda8d3883b0af9f559b435c8
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-05-19 02:03:27 -0700

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" Python script to export 
-data in the CSV format """
+""" Python script to export data in the CSV format """
+
 import csv
 import requests
 import sys
@@ -21,10 +21,10 @@ if __name__ == '__main__':
 
     csv_data = [
             [
-                todo.get["userID"]
-                todo.get["username"]
-                todo.get["completed"]
-                todo.get["title"]
+                employee_id,
+                todo.get["username"],
+                todo.get["completed"],
+                todo.get["title"],
                 ]
             ] for td in todo
 
