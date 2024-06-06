dc681c68a8e61bca3f788983aee5129b0b069c08
KhaledAlshaer <khaledelshaer1172003@gmail.com>
2024-06-06 07:22:57 -0700

    1

--- 0x16-api_advanced/1-top_ten2.py
+++ 0x16-api_advanced/1-top_ten2.py
@@ -15,28 +15,19 @@ def top_ten(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
-        return
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
     response = get(url, headers=user_agent, params=params)
-
-    if response.status_code != 200:
-        print("None")
-        return
+    results = response.json()
 
     try:
-        results = response.json()
-        my_data = results.get('data', {}).get('children', [])
+        my_data = results.get('data').get('children')
 
-        if not my_data:
-            print("None")
-            return
-
-        for item in my_data:
-            print(item.get('data', {}).get('title', "None"))
+        for i in my_data:
+            print(i.get('data').get('title'))
 
     except Exception:
         print("None")
